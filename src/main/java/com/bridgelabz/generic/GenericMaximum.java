88232d7ebb9f383cb56cad1b8df5685469ae10f3
Mohit Shah <mohitshah7777@gmail.com>
2021-04-23 21:28:15 +0530

    Added: Generic Test Maximum program has been Initialized

---
+++ src/main/java/com/bridgelabz/generic/GenericMaximum.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.generic;
+
+public class GenericMaximum {
+}
---
+++ src/test/java/com/bridgelabz/generic/GenericMaximumTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.generic;
+
+import org.junit.Before;
+
+public class GenericMaximumTest {
+
+    GenericMaximum maxObj;
+
+    @Before
+    public void classObject(){
+        maxObj=new GenericMaximum();
+    }
