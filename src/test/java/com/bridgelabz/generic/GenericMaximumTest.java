3c1b2cdd72fc80463463d8e0868d6dd7d8710936
Mohit Shah <mohitshah7777@gmail.com>
2021-04-23 21:35:31 +0530

    Refactor: TC 1.1 - Given Max number at 1st position return same

--- src/test/java/com/bridgelabz/generic/GenericMaximumTest.java
+++ src/test/java/com/bridgelabz/generic/GenericMaximumTest.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.generic;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 public class GenericMaximumTest {
 
@@ -10,4 +12,12 @@ public class GenericMaximumTest {
     public void classObject() {
         maxObj = new GenericMaximum();
     }
+
+    //UC-1 Given 3 Integers find the maximum //TC--1.1 // Integer
+    @Test
+    public void givenMaxIntegerAtFirstPosition_shouldReturnSame(){
+        Integer maxInteger = maxObj.maxPositionInteger(9,5,2);
+        Assert.assertEquals(Integer.valueOf(9),maxInteger);
+    }
+
 }
