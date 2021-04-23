bf0bdba6a679dbdb53d39233c9ae589904be1c69
Mohit Shah <mohitshah7777@gmail.com>
2021-04-23 23:32:24 +0530

    Refactor: to create generic class to take 3 variables

--- src/main/java/com/bridgelabz/generic/GenericMaximum.java
+++ src/main/java/com/bridgelabz/generic/GenericMaximum.java
@@ -1,8 +1,21 @@
 package com.bridgelabz.generic;
 
-public class GenericMaximum {
+//Refactor-2 Refactor to create Generic Class to take in 3 variables of Generic Type
+public class GenericMaximum<E extends Comparable> {
 
-    //Refactor-1 Refactor all the 3 to One Generic Method and find the maximum
+    E firstPosition;
+    E secondPosition;
+    E thirdPosition;
+
+    public GenericMaximum(E firstPosition, E secondPosition, E thirdPosition) {
+        this.firstPosition = firstPosition;
+        this.secondPosition = secondPosition;
+        this.thirdPosition = thirdPosition;
+    }
+
+    public E maximumGeneric(){
+        return genericMaximum(firstPosition,secondPosition,thirdPosition);
+    }
     public <E extends Comparable> E genericMaximum(E firstPosition, E secondPosition, E thirdPosition){
         E maxPosition=firstPosition;
         if(secondPosition.compareTo(maxPosition) > 0)
@@ -10,38 +23,5 @@ public class GenericMaximum {
         if(thirdPosition.compareTo(maxPosition) > 0)
             maxPosition=thirdPosition;
         return maxPosition;
-    }
-
-    //UC-1 Given three Integers Find Maximum
-    //Integer
-    public Integer maxPositionInteger(Integer firstIntPosition, Integer secondIntPosition, Integer thirdIntPosition) {
-        Integer maxInt = firstIntPosition;
-        if (secondIntPosition.compareTo(maxInt) > 0)
-            maxInt = secondIntPosition;
-        if (thirdIntPosition.compareTo(maxInt) > 0)
-            maxInt = thirdIntPosition;
-        return maxInt;
-    }
-
-    //UC-1 Given three Floats Find Maximum
-    //Float
-    public Double maxPositionDouble(Double firstDoublePosition, Double secondDoublePosition, Double thirdDoublePosition) {
-        Double maxDouble = firstDoublePosition;
-        if (secondDoublePosition.compareTo(maxDouble) > 0)
-            maxDouble = secondDoublePosition;
-        if (thirdDoublePosition.compareTo(maxDouble) > 0)
-            maxDouble = thirdDoublePosition;
-        return maxDouble;
-    }
-
-    //UC-3 Given 3 Strings find the maximum
-    //String
-    public String maxPositionString(String firstStringPosition, String secondStringPosition, String thirdStringPosition) {
-        String maxString = firstStringPosition;
-        if (secondStringPosition.compareTo(maxString) > 0)
-            maxString = secondStringPosition;
-        if (thirdStringPosition.compareTo(maxString) > 0)
-            maxString = thirdStringPosition;
-        return maxString;
     }
 }
--- src/test/java/com/bridgelabz/generic/GenericMaximumTest.java
+++ src/test/java/com/bridgelabz/generic/GenericMaximumTest.java
@@ -6,95 +6,30 @@ import org.junit.Test;
 
 public class GenericMaximumTest {
 
-    GenericMaximum maxObj;
+    GenericMaximum maxGenericObject;
 
-    @Before
-    public void classObject() {
-        maxObj = new GenericMaximum();
-    }
-
-    //Refactor-1 Refactor all the 3 to One Generic Method and find the maximum
+    //Refactor-2 Refactor to create Generic Class to take in 3 variables of Generic Type
     // Integer
     @Test
-    public void givenThreeIntegers_shouldReturnMaxPosition(){
-        Integer maxInt = maxObj.genericMaximum(10,7,5);
+    public void givenThreeInteger_shouldReturnMaxPositionGeneric(){
+        maxGenericObject = new GenericMaximum(7,10,5);
+        Integer maxInt = (Integer) maxGenericObject.maximumGeneric();
         Assert.assertEquals(Integer.valueOf(10),maxInt);
     }
 
     //Float
     @Test
-    public void givenThreeFloats_shouldReturnMaxPosition(){
-        Double maxDouble = maxObj.genericMaximum(7.5,10.5,5.5);
+    public void givenThreeFloats_shouldReturnMaxPositionGeneric(){
+        maxGenericObject = new GenericMaximum(7.5,5.5,10.5);
+        Double maxDouble = (Double) maxGenericObject.maximumGeneric();
         Assert.assertEquals(Double.valueOf(10.5),maxDouble);
     }
 
     //String
     @Test
-    public void givenThreeStrings_shouldReturnMaxPosition(){
-        String maxString = maxObj.genericMaximum("Watermelon","Banana","Apple");
+    public void givenThreeStrings_shouldReturnMaxPositionGeneric(){
+        maxGenericObject = new GenericMaximum("Apple","Watermelon","Banana");
+        String maxString = (String) maxGenericObject.maximumGeneric();
         Assert.assertEquals(String.valueOf("Watermelon"),maxString);
-    }
-
-    //UC-1 Given 3 Integers find the maximum //TC--1.1 // Integer
-    @Test
-    public void givenMaxIntegerAtFirstPosition_shouldReturnSame(){
-        Integer maxInteger = maxObj.maxPositionInteger(9,5,2);
-        Assert.assertEquals(Integer.valueOf(9),maxInteger);
-    }
-
-    //TC--1.2
-    @Test
-    public void givenMaxIntegerAtSecondPosition_shouldReturnSame(){
-        Integer maxInteger = maxObj.maxPositionInteger(5,9,2);
-        Assert.assertEquals(Integer.valueOf(9),maxInteger);
-    }
-
-    //TC--1.3
-    @Test
-    public void givenMaxIntegerAtThirdPosition_shouldReturnSame(){
-        Integer maxInteger=maxObj.maxPositionInteger(2,5,9);
-        Assert.assertEquals(Integer.valueOf(9),maxInteger);
-    }
-
-    //UC-2 Given 3 Floats find the maximum //TC--2.1 //Floats
-    @Test
-    public void givenMaxFloatAtFirstPosition_shouldReturnSame(){
-        Double maxDouble = maxObj.maxPositionDouble(2.5,2.3,2.1);
-        Assert.assertEquals(Double.valueOf(2.5),maxDouble);
-    }
-
-    //TC--2.2
-    @Test
-    public void givenMaxFloatAtSecondPosition_shouldReturnSame(){
-        Double maxDouble = maxObj.maxPositionDouble(2.3,2.5,2.1);
-        Assert.assertEquals(Double.valueOf(2.5),maxDouble);
-    }
-
-    //TC--2.3
-    @Test
-    public void givenMaxFloatAtThirdPosition_shouldReturnSame(){
-        Double maxDouble=maxObj.maxPositionDouble(2.1,2.3,2.5);
-        Assert.assertEquals(Double.valueOf(2.5),maxDouble);
-    }
-
-    //UC-3 Given 3 Strings find the maximum //TC--3.1 //String
-    @Test
-    public void givenMaxStringAtFirstPosition_shouldReturnSame(){
-        String maxString = maxObj.maxPositionString("PineApple","Apple","Banana");
-        Assert.assertEquals(String.valueOf("PineApple"),maxString);
-    }
-
-    //TC--3.2
-    @Test
-    public void givenMaxStringAtSecondPosition_shouldReturnSame(){
-        String maxString = maxObj.maxPositionString("Apple","PineApple","Banana");
-        Assert.assertEquals(String.valueOf("PineApple"),maxString);
-    }
-
-    //TC--3.3
-    @Test
-    public void givenMaxStringAtThirdPosition_shouldReturnSame(){
-        String maxString=maxObj.maxPositionString("Apple","Banana","PineApple");
-        Assert.assertEquals(String.valueOf("PineApple"),maxString);
     }
 }
